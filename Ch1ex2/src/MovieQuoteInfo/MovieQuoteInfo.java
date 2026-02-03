05d03747424fc68e107b5a110bd0e445f493a36f
abake974 <abake974@cougarmail.cccc.edu>
2026-02-03 13:36:06 -0500

    Add files via upload

---
+++ Ch1ex2/src/MovieQuoteInfo/MovieQuoteInfo.java
@@ -0,0 +1,13 @@
+package MovieQuoteInfo;
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		System.out.println("You're a wizard, Harry.");
+		System.out.println("Harry Potter and the Sorcerer's Stone");
+		System.out.println("Rubeus Hagrid");
+		System.out.println("2001");
+		
+	}
+
+}
